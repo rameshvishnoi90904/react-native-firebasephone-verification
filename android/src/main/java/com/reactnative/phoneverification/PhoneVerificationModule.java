9fc608ba1cb9f2e30e9b19bc614ceb6afed88ad7
Ramesh <ramesh@appivo.com>
2019-01-30 21:38:56 +0530

    typo mistake in package name

--- android/src/main/java/com/reactnative/phoneverification/PhoneVerificationModule.java
+++ android/src/main/java/com/reactnative/phoneverification/PhoneVerificationModule.java
@@ -1,4 +1,4 @@
-package com.reactnative.phoneverfication;
+package com.reactnative.phoneverification;
 
 import android.content.Intent;
 import android.support.annotation.NonNull;
--- android/src/main/java/com/reactnative/phoneverification/PhoneVerificationPackage.java
+++ android/src/main/java/com/reactnative/phoneverification/PhoneVerificationPackage.java
@@ -1,4 +1,4 @@
-package com.reactnative.phoneverfication;
+package com.reactnative.phoneverification;
 
 import com.facebook.react.ReactPackage;
 import com.facebook.react.bridge.NativeModule;
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class PhoneVerficationPackage implements ReactPackage {
+public class PhoneVerificationPackage implements ReactPackage {
     @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
         List<NativeModule> modules = new ArrayList<>();
